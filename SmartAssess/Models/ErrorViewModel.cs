370581926426f58538f2c1ecac8ed828b032ad45
Oleksandr Myroshnychenko <myroshnychenko.o@pg.com>
2024-07-13 16:14:59 +0300

    Add project files.

---
+++ SmartAssess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartAssess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
