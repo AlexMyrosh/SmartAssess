e2d692b550ff3841a2f29fe78eda72697d48fb5b
Oleksandr Myroshnychenko <myroshnychenko.o@pg.com>
2024-09-21 20:44:37 +0300

    Added trash controller

---
+++ SmartAssess/Controllers/TrashController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation_Layer.ViewModels;
+
+namespace Presentation_Layer.Controllers
+{
+    public class TrashController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new TrashViewModel();
+            return View(viewModel);
+        }
+    }
+}
---
+++ SmartAssess/ViewModels/TrashViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation_Layer.ViewModels
+{
+    public class TrashViewModel
+    {
+        List<CourseViewModel> DeletedCourses { get; set; }
+
+        List<ExamViewModel> DeletedExams { get; set; }
+    }
+}
